b0af3cc9daaa269fd25091bc5dfdea63ddf1ea63
Anny Patel <30823493+annypatel@users.noreply.github.com>
2019-11-19 22:48:02 +0530

    Add implementation for market price chart fragment

--- app/src/main/java/bitcoin/app/App.kt
+++ app/src/main/java/bitcoin/app/App.kt
@@ -1,6 +1,7 @@
 package bitcoin.app
 
 import bitcoin.app.di.DaggerAppComponent
+import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
@@ -8,6 +9,11 @@ import dagger.android.support.DaggerApplication
  * Application class for this app. Use this class to do one time initialization.
  */
 class App : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+    }
 
     override fun applicationInjector(): AndroidInjector<App> {
         return DaggerAppComponent.create()
--- charts/ui/src/main/java/bitcoin/charts/ui/MarketPriceChartFragment.kt
+++ charts/ui/src/main/java/bitcoin/charts/ui/MarketPriceChartFragment.kt
@@ -4,9 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import bitcoin.base.ui.observe
+import bitcoin.charts.domain.BaselinedPrices
+import bitcoin.charts.domain.Interval.Companion.days
+import bitcoin.charts.domain.Interval.Companion.years
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis.XAxisPosition.BOTTOM
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
 import dagger.android.support.DaggerFragment
+import org.threeten.bp.temporal.ChronoUnit
 import javax.inject.Inject
 
 /**
@@ -14,15 +29,97 @@ import javax.inject.Inject
  */
 class MarketPriceChartFragment : DaggerFragment() {
 
+    companion object {
+        private val TIMESPAN = years(1)
+        private val ROLLING_AVG = days(1)
+        private val ROLLING_AVG_UNIT = ChronoUnit.DAYS
+    }
+
     @Inject
     lateinit var factory: ViewModelProvider.Factory
     private val viewModel: MarketPriceChartViewModel by viewModels { factory }
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var tvError: TextView
+    private lateinit var chart: LineChart
+
+    private val chartLineWidth by lazy {
+        resources.getDimensionPixelSize(R.dimen.chart_line_width).toFloat()
+    }
+    private val chartLineColor by lazy {
+        getColor(requireContext(), R.color.chart_line_color)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_market_price_chart, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_market_price_chart, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        progressBar = view.findViewById(R.id.progressBar)
+        tvError = view.findViewById(R.id.tvError)
+
+        // configure line chart
+        chart = view.findViewById<LineChart>(R.id.marketPriceChart)
+            .apply {
+                axisRight.isEnabled = false
+                xAxis.position = BOTTOM
+                xAxis.granularity = 1f
+                xAxis.isGranularityEnabled = true
+                xAxis.setDrawGridLines(false)
+                legend.isEnabled = false
+                description.isEnabled = false
+            }
+
+        // observe and get the bitcoin market price
+        viewModel.prices.observe(this, ::showLoading, ::showPriceOnChart, ::showError)
+        savedInstanceState ?: viewModel.getMarketPrice(TIMESPAN, ROLLING_AVG, ROLLING_AVG_UNIT)
+    }
+
+    private fun showLoading() {
+        progressBar.visibility = View.VISIBLE
+        tvError.visibility = View.GONE
+        chart.visibility = View.GONE
+    }
+
+    private fun showPriceOnChart(prices: BaselinedPrices) {
+        // convert values on x-axis to date
+        chart.xAxis.valueFormatter = object : ValueFormatter() {
+            override fun getFormattedValue(value: Float): String =
+                viewModel.convertToDate(
+                    value.toLong(),
+                    ROLLING_AVG_UNIT,
+                    prices.baseline.timestamp
+                )
+        }
+
+        // create data-set
+        val entries = prices.prices.map {
+            Entry(
+                it.duration.toFloat(),
+                it.price.price,
+                it.price
+            )
+        }
+        val dataSet = LineDataSet(entries, "")
+            .apply {
+                lineWidth = chartLineWidth
+                color = chartLineColor
+                setDrawCircles(false)
+                setDrawValues(false)
+            }
+
+        // show price in chart
+        progressBar.visibility = View.GONE
+        chart.visibility = View.VISIBLE
+        chart.data = LineData(dataSet)
+        chart.invalidate()
+    }
+
+    private fun showError(@StringRes resId: Int) {
+        progressBar.visibility = View.GONE
+        tvError.visibility = View.VISIBLE
+        tvError.setText(resId)
     }
 }
